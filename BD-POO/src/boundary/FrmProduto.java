68f68be2924ef741f4bb86b6cbeb5ff2cbb53b60
BlueTub <hury_gabriel@hotmail.com>
2015-05-18 20:17:43 -0300

    frmproduto

--- BD-POO/src/boundary/FrmProduto.java
+++ BD-POO/src/boundary/FrmProduto.java
@@ -1,13 +1,32 @@
 package boundary;
 
+
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class FrmProduto {
 
-	
+	private JFrame janela = new JFrame("Produto");
+
+	public FrmProduto(){
+		
+		JPanel panPrincipal = new JPanel();
+		panPrincipal.setLayout(new BorderLayout());
+		JPanel panForm = new JPanel();
+		
+		janela.setContentPane( panPrincipal);
+		janela.setSize(300,200);
+		janela.setVisible(true);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	
 	
 	public static void main(String[] args) {
-		
 
+		new FrmProduto();
 	}
 
 }

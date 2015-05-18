33d40cfff46a8f6b1e3fd6dfe713191315f182e5
Pedro <Pedro@Pedro-PC>
2015-05-18 20:39:30 -0300

    Funcionario

---
+++ BD-POO/src/entity/Funcionario.java
@@ -0,0 +1,30 @@
+package entity;
+
+public class Funcionario {
+	
+	private long id;
+	private String nome;
+	private int cpf;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}

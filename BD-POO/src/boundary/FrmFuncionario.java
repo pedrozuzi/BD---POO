e005236b5faf08a28f666991854985c715302a66
BlueTub <hury_gabriel@hotmail.com>
2015-05-18 20:41:01 -0300

    Atualização para windowBuilder

--- BD-POO/src/boundary/FrmFuncionario.java
+++ BD-POO/src/boundary/FrmFuncionario.java
@@ -1,5 +1,29 @@
 package boundary;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class FrmFuncionario {
 
+	private JFrame janela = new JFrame("Produto");
+
+	public FrmFuncionario(){
+		
+		JPanel panPrincipal = new JPanel();
+		JPanel panForm = new JPanel();
+		
+		janela.setContentPane( panPrincipal);
+		panPrincipal.setLayout(null);
+		janela.setSize(400,281);
+		janela.setVisible(true);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	public static void main(String[] args) {
+
+		new FrmFuncionario();
+	}
+	
+	
 }

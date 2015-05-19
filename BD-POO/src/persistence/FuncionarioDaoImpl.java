2aab390538e0833374fe6b65e386ecf61c60d2a4
pepeu07 <Pedro Afonso@PEDRO>
2015-05-19 20:38:36 -0300

    FuncionarioDaoImpl

--- BD-POO/src/persistence/FuncionarioDaoImpl.java
+++ BD-POO/src/persistence/FuncionarioDaoImpl.java
@@ -2,6 +2,7 @@ package persistence;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import connection.ConnectionImpl;
@@ -33,7 +34,22 @@ public class FuncionarioDaoImpl implements FuncionarioDao {
 
 	@Override
 	public void atualizarFuncionario(Funcionario func) throws SQLException {
-		
+		String query = "update funcionario set "
+				+ "cpf = ?,"
+				+ "nome = ?,"
+				+ "cargo = ?,"
+				+ "salario= ?"
+				+ "where id = ?";
+				
+		PreparedStatement ps = c.prepareStatement( query );
+		ps.setString(1, func.getCpf() );
+		ps.setString(2, func.getNome() );
+		ps.setString(3, func.getCargo() );
+		ps.setDouble(4, func.getSalario() );
+		ps.setInt(5, func.getId() );
+		ps.execute();
+		System.out.println("Dados atualizados");
+		ps.close();
 	}
 
 	@Override
@@ -50,7 +66,21 @@ public class FuncionarioDaoImpl implements FuncionarioDao {
 	@Override
 	public Funcionario consultarFuncionario(Funcionario func)
 			throws SQLException {
-		return null;
+		
+		String query = "select id, cpf, nome, cargo, salario "
+				+ "from funcionario where id = ?";
+		PreparedStatement ps = c.prepareStatement( query );
+		ps.setInt(1,  func.getId() );
+		ResultSet rs = ps.executeQuery();
+		if( rs.next() ){
+			func.setId( rs.getInt("id") );
+			func.setCpf( rs.getString("cpf") );
+			func.setNome( rs.getString("nome") );	
+			func.setCargo( rs.getString("cargo"));
+			func.setSalario( rs.getDouble("salario"));
+		}
+		ps.close();
+		return func;
 	}
 
 }

7c8590356d0ba7bf3128adcf0b479a74fb2c8b73
Pedro <Pedro@Pedro-PC>
2015-05-18 21:56:01 -0300

    FrmFuncionario

--- BD-POO/src/boundary/FrmFuncionario.java
+++ BD-POO/src/boundary/FrmFuncionario.java
@@ -2,10 +2,18 @@ package boundary;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
 
 public class FrmFuncionario {
 
 	private JFrame janela = new JFrame("Cadastro de Funcionário");
+	private JTextField txtCod;
+	private JTextField txtCpf;
+	private JTextField txtNome;
+	private JTextField txtCargo;
+	private JTextField txtSalario;
 
 	public FrmFuncionario() {
 		
@@ -14,7 +22,63 @@ public class FrmFuncionario {
 		
 		janela.setContentPane( panPrincipal );
 		panPrincipal.setLayout(null);
-		janela.setSize(450,350);
+		
+		JLabel lblCod = new JLabel("Código: ");
+		lblCod.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblCod.setBounds(10, 41, 54, 18);
+		panPrincipal.add(lblCod); //////////////////
+		
+		txtCod = new JTextField();
+		txtCod.setFont(new Font("Arial", Font.PLAIN, 14));
+		txtCod.setBounds(74, 38, 86, 27);
+		txtCod.setColumns(10);
+		panPrincipal.add(txtCod);    /////////////////
+		
+		JLabel lblCpf = new JLabel("CPF: ");
+		lblCpf.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblCpf.setBounds(10, 88, 46, 14);
+		panPrincipal.add(lblCpf);     ///////////////////
+		
+		txtCpf = new JTextField();
+		txtCpf.setFont(new Font("Arial", Font.PLAIN, 14));
+		txtCpf.setBounds(74, 79, 124, 27);
+		panPrincipal.add(txtCpf);        ///////
+		txtCpf.setColumns(10);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblNome.setBounds(10, 123, 46, 14);
+		panPrincipal.add(lblNome);    //////////////
+		
+		txtNome = new JTextField();
+		txtNome.setFont(new Font("Arial", Font.PLAIN, 14));
+		txtNome.setBounds(74, 117, 350, 27);
+		panPrincipal.add(txtNome);  ///////////
+		txtNome.setColumns(10);
+		
+		JLabel lblCargo = new JLabel("Cargo:");
+		lblCargo.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblCargo.setBounds(10, 162, 46, 18);
+		panPrincipal.add(lblCargo); //////////
+		
+		txtCargo = new JTextField();
+		txtCargo.setFont(new Font("Arial", Font.PLAIN, 14));
+		txtCargo.setBounds(74, 159, 350, 27);
+		panPrincipal.add(txtCargo); /////////////
+		txtCargo.setColumns(10);
+		
+		JLabel lblSalrio = new JLabel("Salário:");
+		lblSalrio.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblSalrio.setBounds(10, 200, 62, 17);
+		panPrincipal.add(lblSalrio);   /////////////
+		
+		txtSalario = new JTextField();
+		txtSalario.setFont(new Font("Arial", Font.PLAIN, 14));
+		txtSalario.setBounds(74, 196, 107, 27);
+		panPrincipal.add(txtSalario);  /////////////
+		txtSalario.setColumns(10);
+		
+		janela.setSize(450,341);
 		janela.setVisible(true);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
@@ -24,6 +88,4 @@ public class FrmFuncionario {
 
 		new FrmFuncionario();
 	}
-	
-	
 }

4d5f9e941600a5e527dcaae6c158338199a4aff3
BlueTub <hury_gabriel@hotmail.com>
2015-05-18 23:01:59 -0300

    ;

--- BD-POO/src/boundary/FrmProduto.java
+++ BD-POO/src/boundary/FrmProduto.java
@@ -2,8 +2,6 @@ package boundary;
 
 
 
-
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
@@ -18,6 +16,14 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JButton;
 import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.JScrollBar;
+
+/**
+ * 
+ * @author Hury
+ *
+ */
 
 public class FrmProduto {
 
@@ -33,6 +39,7 @@ public class FrmProduto {
 	private JLabel lblId_1;
 	private JLabel lblDataDeValidade;
 	private JTextField textField_4;
+	private JTable table;
 
 	public FrmProduto(){
 		
@@ -62,7 +69,7 @@ public class FrmProduto {
 		
 		JPanel panProduto = new JPanel();
 		panProduto.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Produto", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panProduto.setBounds(38, 54, 529, 136);
+		panProduto.setBounds(38, 213, 529, 136);
 		panPrincipal.add(panProduto);
 		panProduto.setLayout(null);
 		panProduto.setOpaque(false);
@@ -106,7 +113,7 @@ public class FrmProduto {
 		
 		panLote = new JPanel();
 		panLote.setBorder(new TitledBorder(null, "Lote", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panLote.setBounds(38, 201, 529, 100);
+		panLote.setBounds(38, 360, 529, 100);
 		panPrincipal.add(panLote);
 		panLote.setLayout(null);
 		panLote.setOpaque(false);
@@ -125,35 +132,90 @@ public class FrmProduto {
 		panLote.add(lblDataDeValidade);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(104, 50, 106, 25);
+		textField_2.setBounds(108, 52, 106, 20);
 		panLote.add(textField_2);
 		textField_2.setColumns(10);
 		
 		JButton btnNewButton_1 = new JButton("");
 		btnNewButton_1.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniSalvar.png")));
-		btnNewButton_1.setBounds(487, 326, 69, 23);
+		btnNewButton_1.setBounds(38, 167, 69, 23);
 		panPrincipal.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("");
 		btnNewButton_2.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniX.png")));
-		btnNewButton_2.setBounds(383, 326, 69, 23);
+		btnNewButton_2.setBounds(124, 167, 69, 23);
 		panPrincipal.add(btnNewButton_2);
 		
-		JLabel lblAdicionar = new JLabel("Adicionar");
-		lblAdicionar.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblAdicionar.setBounds(497, 350, 69, 23);
-		panPrincipal.add(lblAdicionar);
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(548, 23, 17, 90);
+		panPrincipal.add(scrollBar);
 		
-		JLabel lblLimparCampos = new JLabel("Limpar Campos");
-		lblLimparCampos.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblLimparCampos.setBounds(372, 350, 103, 23);
-		panPrincipal.add(lblLimparCampos);
+		table = new JTable();
+		table.setBounds(38, 23, 529, 90);
+		panPrincipal.add(table);
+		
+		JLabel lblRegistros = new JLabel("Registros");
+		lblRegistros.setBounds(38, 0, 79, 23);
+		panPrincipal.add(lblRegistros);
+		
+		JButton button = new JButton("");
+		button.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniEditar.png")));
+		button.setBounds(210, 167, 69, 23);
+		panPrincipal.add(button);
+		
+		JButton button_1 = new JButton("");
+		button_1.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniRewind.png")));
+		button_1.setBounds(151, 115, 69, 23);
+		panPrincipal.add(button_1);
+		
+		JButton button_2 = new JButton("");
+		button_2.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniBack.png")));
+		button_2.setBounds(230, 115, 69, 23);
+		panPrincipal.add(button_2);
+		
+		JButton button_3 = new JButton("");
+		button_3.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniNext.png")));
+		button_3.setBounds(306, 115, 69, 23);
+		panPrincipal.add(button_3);
+		
+		JButton button_4 = new JButton("");
+		button_4.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniFoward.png")));
+		button_4.setBounds(385, 115, 69, 23);
+		panPrincipal.add(button_4);
+		
+		JLabel lblNewLabel = new JLabel("Ultimo");
+		lblNewLabel.setBounds(171, 137, 40, 19);
+		panPrincipal.add(lblNewLabel);
+		
+		JLabel lblAnterior = new JLabel("Anterior");
+		lblAnterior.setBounds(240, 137, 47, 19);
+		panPrincipal.add(lblAnterior);
+		
+		JLabel lblProximo = new JLabel("Proximo");
+		lblProximo.setBounds(316, 137, 47, 19);
+		panPrincipal.add(lblProximo);
+		
+		JLabel lblPrimeiro = new JLabel("Primeiro");
+		lblPrimeiro.setBounds(395, 137, 54, 19);
+		panPrincipal.add(lblPrimeiro);
+		
+		JLabel lblSalvar = new JLabel("Salvar");
+		lblSalvar.setBounds(48, 188, 40, 19);
+		panPrincipal.add(lblSalvar);
+		
+		JLabel lblExcluir = new JLabel("Excluir");
+		lblExcluir.setBounds(134, 188, 40, 19);
+		panPrincipal.add(lblExcluir);
+		
+		JLabel lblEditar = new JLabel("Editar");
+		lblEditar.setBounds(220, 188, 40, 19);
+		panPrincipal.add(lblEditar);
 		
 		JLabel lblBackGround = new JLabel("New label");
 		lblBackGround.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/BackGround.png")));
-		lblBackGround.setBounds(0, 0, 606, 429);
+		lblBackGround.setBounds(0, 0, 594, 481);
 		panPrincipal.add(lblBackGround);
-		janela.setSize(622,489);
+		janela.setSize(605,531);
 		janela.setVisible(true);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}

8de1bf6484e8e830fc69853bf2a972d9f851dae6
Pedro <Pedro@Pedro-PC>
2015-05-19 21:51:44 -0300

    Login

--- BD-POO/src/boundary/FrmLogin.java
+++ BD-POO/src/boundary/FrmLogin.java
@@ -2,6 +2,11 @@ package boundary;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 /**
  * 
@@ -12,6 +17,11 @@ import javax.swing.JPanel;
 public class FrmLogin {
 
 	private JFrame janela = new JFrame("Produto");
+	private JTextField txtUsuario;
+	private JPasswordField pwdSenha;
+	private JLabel lblUsuario;
+	private JLabel lblSenha;
+	private JButton btnLogar;
 
 	public FrmLogin(){
 		
@@ -20,6 +30,32 @@ public class FrmLogin {
 		
 		janela.setContentPane( panPrincipal);
 		panPrincipal.setLayout(null);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setFont(new Font("Arial", Font.PLAIN, 14));
+		txtUsuario.setBounds(95, 61, 241, 28);
+		panPrincipal.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		pwdSenha = new JPasswordField();
+		pwdSenha.setFont(new Font("Arial", Font.PLAIN, 14));
+		pwdSenha.setBounds(95, 124, 241, 28);
+		panPrincipal.add(pwdSenha);
+		
+		lblUsuario = new JLabel("Usuário:");
+		lblUsuario.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblUsuario.setBounds(10, 67, 61, 14);
+		panPrincipal.add(lblUsuario);
+		
+		lblSenha = new JLabel("Senha:");
+		lblSenha.setFont(new Font("Arial", Font.PLAIN, 14));
+		lblSenha.setBounds(10, 130, 61, 14);
+		panPrincipal.add(lblSenha);
+		
+		btnLogar = new JButton("Logar");
+		btnLogar.setFont(new Font("Arial", Font.PLAIN, 14));
+		btnLogar.setBounds(247, 174, 89, 43);
+		panPrincipal.add(btnLogar);
 		janela.setSize(400,281);
 		janela.setVisible(true);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -30,6 +66,4 @@ public class FrmLogin {
 
 		new FrmLogin();
 	}
-	
-	
 }

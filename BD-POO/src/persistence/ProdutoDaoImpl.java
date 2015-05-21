1061fe1de680c178e8cff9e9237607db118b004e
BlueTub <hury_gabriel@hotmail.com>
2015-05-20 22:02:33 -0300

    ;

--- BD-POO/src/persistence/ProdutoDaoImpl.java
+++ BD-POO/src/persistence/ProdutoDaoImpl.java
@@ -19,13 +19,51 @@ public class ProdutoDaoImpl implements ProdutoDao {
 		c = gDao.getConnection();
 	}
 	
-	
+	/**
+	 * create table lote(
+id int identity not null,
+data_validade datetime not null
+primary key(id))
+
+create table produto(
+id int identity not null,
+nome varchar(60) not null,
+descricao varchar(60),
+id_fornecedor int not null,
+valor_venda int not null,
+valor_compra int not null,
+id_lote int not null
+primary key(id)
+foreign key(id_fornecedor) references fornecedor(id),
+foreign key(id_lote) references lote(id))
+
+create table lote_produto(
+idProduto int not null,
+idLote int not null,
+primary key(idProduto, idLote),
+foreign key(idLote)references lote(id),
+foreign key(idProduto)references produto(id))
+	 */
 
 
 //
 	@Override
 	public void insereProduto(Produto prod) throws SQLException {
-		// TODO Auto-generated method stub
+	//	String sql = "INSERT INTO materias (nome, carga_horaria)"+
+	//			"VALUES(?,?)";
+	//	PreparedStatement ps = c.prepareStatement(sql);
+	//	ps.setString(1, mat.getNome());
+		//ps.setInt(2, mat.getCargaHoraria());
+		//ps.execute();
+		//ps.close();
+		
+		String sql = "INSERT INTO produto (nome,descricao,id_fornecedor,valor_venda,valor_compra,)"+
+		"VALUES(?,?,?,?,?)";
+		PreparedStatement ps = c.prepareStatement(sql);
+		ps.setString(1, prod.getNome());
+		ps.setString(2, prod.getDescricao());
+		ps.setInt(3, prod.getId_fornecedor());
+		
 		
 	}
 

5f9d4d64290caba85d55db107c6064a2b2763cad
pepeu07 <Pedro Afonso@PEDRO>
2015-05-21 21:30:17 -0300

    Atualizacao Funcionario

--- BD-POO/src/control/CtrlFuncionario.java
+++ BD-POO/src/control/CtrlFuncionario.java
@@ -1,9 +1,12 @@
 package control;
 
+import persistence.FuncionarioDao;
 import entity.Funcionario;
 
 public class CtrlFuncionario {
 
+	private FuncionarioDao fDao;
+	
 	public void incluir(Funcionario f){
 		
 	}

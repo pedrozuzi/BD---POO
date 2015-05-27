19a3d318c6b5b66b974cc24c518383253957ec04
Pedro <Pedro@Pedro-PC>
2015-05-26 23:42:00 -0300

    Ajustes tela Fornecedor

--- BD-POO/src/boundary/FrmFornecedor.java
+++ BD-POO/src/boundary/FrmFornecedor.java
@@ -5,42 +5,32 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import control.ConfigTelas;
 import control.CtrlFornecedor;
 import control.CtrlTabela;
 import control.CtrlTableFornecedor;
 import control.TratamentoTextFields;
-
 import javax.swing.JLabel;
-
 import java.awt.Color;
-
 import javax.swing.border.TitledBorder;
 import javax.swing.UIManager;
 import javax.swing.JButton;
-
 import java.awt.Font;
-
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
 import entity.Fornecedor;
-
 import java.awt.SystemColor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.table.DefaultTableModel;
 
 public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 	
 	private JFrame janela; 
 	private JPanel panPrincipal;
-	private JTextField txtId;
 	private JTextField txtNome;
 	private JTextField txtTelefone;
 	private JLabel lblIncluir;
@@ -49,7 +39,6 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 	private JLabel lblExcluir;
 	private JLabel lblPesquisar;
 	private JLabel lblTelefone;
-	private JLabel lblId;
 	private JLabel lblNome;
 	private JButton btnIncluir;
 	private JButton btnAlterar;
@@ -65,6 +54,8 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 	private CtrlTableFornecedor controlTable;
 	private JButton btnLupaPesquisar;
 	private DefaultTableModel modelo;
+	private int id;
+	private List<Fornecedor> lista;
 	
 	public FrmFornecedor() {
 		janela = new JFrame("Fornecedor");
@@ -76,7 +67,7 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 		
 		btnLupaPesquisar = new JButton("");
 		btnLupaPesquisar.setIcon(new ImageIcon(FrmProduto.class.getResource("/img/MiniLupa.png")));
-		btnLupaPesquisar.setBounds(465, 319, 65, 31);
+		btnLupaPesquisar.setBounds(447, 291, 65, 31);
 		btnLupaPesquisar.setVisible(false);
 		panPrincipal.add(btnLupaPesquisar);
 		
@@ -141,38 +132,25 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 		btnPesquisar.setBounds(326, 11, 69, 41);
 		panel.add(btnPesquisar);
 		
-		txtId = new JTextField();
-		txtId.setFont(new Font("Tahoma", Font.BOLD, 11));
-		txtId.setBounds(137, 319, 109, 20);
-		txtId.setVisible(false);
-		txtId.setEnabled(false);
-		panPrincipal.add(txtId);
-		txtId.setColumns(10);
-		
 		txtNome = new TratamentoTextFields();
-		txtNome.setBounds(137, 360, 296, 20);
+		txtNome.setBounds(126, 302, 296, 20);
 		txtNome.setVisible(false);
 		panPrincipal.add(txtNome);
 		txtNome.setColumns(10);
 		
-		lblId = new JLabel("C\u00F3digo:");
-		lblId.setBounds(62, 322, 65, 14);
-		lblId.setVisible(false);
-		panPrincipal.add(lblId);
-		
 		lblNome = new JLabel("Nome:");
-		lblNome.setBounds(62, 363, 65, 14);
+		lblNome.setBounds(51, 305, 65, 14);
 		lblNome.setVisible(false);
 		panPrincipal.add(lblNome);
 		
 		txtTelefone = new TratamentoTextFields(9);
-		txtTelefone.setBounds(137, 406, 109, 20);
+		txtTelefone.setBounds(126, 348, 109, 20);
 		txtTelefone.setVisible(false);
 		panPrincipal.add(txtTelefone);
 		txtTelefone.setColumns(10);
 		
 		lblTelefone = new JLabel("Telefone:");
-		lblTelefone.setBounds(62, 409, 58, 14);
+		lblTelefone.setBounds(51, 351, 58, 14);
 		lblTelefone.setVisible(false);
 		panPrincipal.add(lblTelefone);
 		
@@ -181,14 +159,14 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 				("/img/MiniSalvar.png")));
 		btnGravar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnGravar.setVisible(false);
-		btnGravar.setBounds(434, 460, 96, 31);
+		btnGravar.setBounds(434, 416, 96, 31);
 		panPrincipal.add(btnGravar);
 		
 		btnLimpar = new JButton("Limpar");
 		btnLimpar.setIcon(new ImageIcon(FrmFornecedor.class.getResource
 				("/img/MiniClear.png")));
 		btnLimpar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnLimpar.setBounds(240, 460, 96, 31);
+		btnLimpar.setBounds(240, 416, 96, 31);
 		btnLimpar.setVisible(false);
 		panPrincipal.add(btnLimpar);
 		
@@ -196,14 +174,14 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 		btnVoltar.setIcon(new ImageIcon(FrmFornecedor.class.getResource
 				("/img/MiniBack.png")));
 		btnVoltar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnVoltar.setBounds(46, 460, 96, 31);
+		btnVoltar.setBounds(46, 416, 96, 31);
 		btnVoltar.setVisible(false);
 		panPrincipal.add(btnVoltar);
 		
 		lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon(FrmFuncionario.class.getResource
 				("/img/LogoLudpet.png")));
-		lblLogo.setBounds(10, 181, 546, 199);
+		lblLogo.setBounds(13, 135, 546, 199);
 		panPrincipal.add(lblLogo);
 		
 		table = new JTable();
@@ -214,7 +192,7 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 		
 		modelo = montarTabela();
 		
-		janela.setSize(585,553);
+		janela.setSize(581,511);
 		janela.setVisible(true);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		centralizarFrame(janela);
@@ -233,7 +211,7 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 					("/img/MiniSalvar.png")));
 			btnGravar.setText("Alterar");
 			btnGravar.setActionCommand("Alterar");
-			telaAlterarExcluirFornecedor();
+			telaAlterarExcluirPesquisarFornecedor();
 			
 		});
 		
@@ -253,11 +231,13 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 			btnGravar.setIcon(new ImageIcon(FrmFornecedor.class.getResource
 					("/img/trash.png")));
 			btnGravar.setActionCommand("Excluir");
-			telaAlterarExcluirFornecedor();
+			telaAlterarExcluirPesquisarFornecedor();
 		});
 		
 		btnPesquisar.addActionListener(e -> {
+			btnLupaPesquisar.setVisible(true);
 			btnGravar.setEnabled(false);
+			telaAlterarExcluirPesquisarFornecedor();
 		});
 		
 		btnGravar.addActionListener(e -> {
@@ -272,30 +252,32 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 		Fornecedor f = new Fornecedor();
 		control = new CtrlFornecedor();
 		if("Alterar".equalsIgnoreCase(cmd)){
-			f.setId(Integer.parseInt(txtId.getText()));
+			f.setId(id);
 			f.setNome(txtNome.getText());
 			f.setTelefone(Integer.parseInt(txtTelefone.getText()));
 			control.atualiza(f);
+			modelo.setNumRows(0); //apagar Jtable para uma nova consulta
+			//buscarDadosTabelaPorNome(modelo);
 		}else if("Incluir".equalsIgnoreCase(cmd)){
 			f.setIdTipo(4);
 			f.setNome(txtNome.getText());
 			f.setTelefone(Integer.parseInt(txtTelefone.getText()));
 			control.inserir(f);
 		}else if("Excluir".equalsIgnoreCase(cmd)){
-			f.setId(Integer.parseInt(txtId.getText()));
+			f.setId(id);
 			f.setNome(txtNome.getText());
 			f.setTelefone(Integer.parseInt(txtTelefone.getText()));
 			control.excluir(f);
+			modelo.setNumRows(0); //apagar Jtable para uma nova consulta
+			//buscarDadosTabelaPorNome(modelo);
 		}
 		limpaCampos();
 	}
 
-	private void telaAlterarExcluirFornecedor() {
+	private void telaAlterarExcluirPesquisarFornecedor() {
 		btnLupaPesquisar.setVisible(true);
-		txtId.setVisible(true);
 		txtNome.setVisible(true);
 		txtTelefone.setVisible(true);
-		lblId.setVisible(true);
 		lblNome.setVisible(true);
 		lblTelefone.setVisible(true);
 		btnGravar.setVisible(true);
@@ -309,15 +291,11 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 
 	private void limpaCampos() {
 		txtNome.setText("");
-		txtId.setText("");
 		txtTelefone.setText("");
 	}
 
 	private void telaInserirFornecedor(){
 		btnLupaPesquisar.setVisible(false);
-		txtId.setVisible(false);
-		txtId.setEnabled(false);
-		lblId.setVisible(false);
 		txtNome.setVisible(true);
 		txtTelefone.setVisible(true);
 		lblNome.setVisible(true);
@@ -332,17 +310,17 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		Object[] valores = new Object[3];
+		Object[] valores = new Object[2];
 		int linha = table.getSelectedRow();
 		int coluna = table.getSelectedColumn();
 		
 		for (coluna = 0; coluna < table.getColumnCount(); coluna++) {
 			valores[coluna] = table.getValueAt(linha, coluna);
 		}
-
-			txtId.setText( String.valueOf(valores[0]));
-			txtNome.setText( String.valueOf(valores[1]));
-			txtTelefone.setText(String.valueOf(valores[2])); 
+		
+			id = lista.get(linha).getId();
+			txtNome.setText( String.valueOf(valores[0]));
+			txtTelefone.setText(String.valueOf(valores[1])); 
 
 	}
 	
@@ -351,10 +329,9 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 	}
 	
 	public DefaultTableModel montarTabela () {
-			String[] colunas = new String[3];
-			colunas[0] = "Código";
-			colunas[1] = "Nome";
-			colunas[2] = "Telefone";
+			String[] colunas = new String[2];
+			colunas[0] = "Nome";
+			colunas[1] = "Telefone";
 
 			modelo = new CtrlTabela(new Object[][] {}, colunas);
 
@@ -362,8 +339,8 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 			table.addMouseListener(this);
 			table.getTableHeader().setReorderingAllowed(false); //deixar as colunas para nao serem movidas de seu lugar original
 			table.getColumnModel().getColumn(0).setResizable(false);
-			table.getColumnModel().getColumn(1).setPreferredWidth(268);
-			table.getColumnModel().getColumn(2).setPreferredWidth(143);
+			table.getColumnModel().getColumn(0).setPreferredWidth(250);
+			table.getColumnModel().getColumn(1).setPreferredWidth(60);
 			table.setVisible(false);
 			scrollPane.setViewportView(table);
 			return modelo;
@@ -371,18 +348,16 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 	
 	public void buscarDadosTabelaPorNome(DefaultTableModel modelo) {
 		controlTable = new CtrlFornecedor(); //instanciado comoa tribulto
-		List<Fornecedor> lista = new ArrayList<Fornecedor>();
+		lista = new ArrayList<Fornecedor>();
 		
-		if (txtId.getText().equals("")) {
 			if (!txtNome.getText().equals("")) {
 				try {
 					lista = controlTable.buscaFornecedorPorNome(txtNome.getText());
 					if (!lista.isEmpty()) {
 						for (Fornecedor f : lista) {
-							Object[] linha = new Object[3];
-							linha[0] = f.getId();
-							linha[1] = f.getNome();
-							linha[2] = f.getTelefone();
+							Object[] linha = new Object[2];
+							linha[0] = f.getNome();
+							linha[1] = f.getTelefone();
 							modelo.addRow(linha);
 						} 
 					}else{
@@ -394,9 +369,8 @@ public class FrmFornecedor extends MouseAdapter implements ConfigTelas{
 					e.printStackTrace();
 				}
 			}else{
-				System.out.println("campo vazio");
+				JOptionPane.showMessageDialog(null, "Digite um nome para pesquisar",
+						"Aviso", JOptionPane.INFORMATION_MESSAGE);
 			}
 		}
-	}
-	
 }

d4e58ff19c26c379d1bb1ecfced7158f93eeb6c6
Pedro <Pedro@Pedro-PC>
2015-05-27 22:43:19 -0300

    Entidade Animal

---
+++ BD-POO/src/entity/Animal.java
@@ -0,0 +1,51 @@
+package entity;
+
+import boundary.Especie;
+
+public class Animal {
+	
+	private int id;
+	private String rga;
+	private String nome;
+	private String raca;
+	private Especie especie;
+	private String cor;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRga() {
+		return rga;
+	}
+	public void setRga(String rga) {
+		this.rga = rga;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRaca() {
+		return raca;
+	}
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+	public Especie getEspecie() {
+		return especie;
+	}
+	public void setEspecie(Especie especie) {
+		this.especie = especie;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+}

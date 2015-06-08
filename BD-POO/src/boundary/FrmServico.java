e51b498231f524071941b2618d85ed238e9ed634
pepeu07 <Pedro Afonso@PEDRO>
2015-06-08 20:33:22 -0300

    FrmServico

---
+++ BD-POO/src/boundary/FrmServico.java
@@ -0,0 +1,18 @@
+package boundary;
+
+import javax.swing.JFrame;
+
+public class FrmServico {
+
+	
+	public FrmServico() {
+		
+		JFrame janela = new JFrame("Servico");
+		
+	
+	}
+	
+	public static void main(String[] args) {
+		new FrmServico();
+	}
+}

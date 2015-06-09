3a9fe2b251ec7def2d8dfa0644f20ea328fa63b5
Pedro <Pedro@Pedro-PC>
2015-06-09 19:54:50 -0300

    Entidade Agenda

--- BD-POO/src/entity/Agenda.java
+++ BD-POO/src/entity/Agenda.java
@@ -1,22 +1,33 @@
 package entity;
 
-import java.util.Date;
-
 public class Agenda {
 	
 	private String horario;
-	private Boolean servico;
+	private Boolean escolhaHorario;
 	private String descricao;
+	private Servico servico;
+	private Cliente cliente;
 	
 	@Coluna(nome="Serviço", posicao=1)
-	public Boolean getServico() {
+	public Boolean getEscolhaHorario() {
+		return escolhaHorario;
+	}
+	public void setEscolhaHorario(Boolean escolhaHorario) {
+		this.escolhaHorario = escolhaHorario;
+	}
+	public Servico getServico() {
 		return servico;
 	}
-	
-	public void setServico(Boolean boolean1) {
-		this.servico = boolean1;
+	public void setServico(Servico servico) {
+		this.servico = servico;
 	}
-	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
 	@Coluna(nome="Tipo de Serviço", posicao=2)
 	public String getDescricao() {
 		return descricao;

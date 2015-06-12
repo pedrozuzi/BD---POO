08d82cd79ed775a2c1088eec647b72c13b67546d
tecnico <tecnico@10.68.76.173>
2015-06-12 17:22:39 -0300

    s s

--- BD-POO/src/control/CtrlTableProduto.java
+++ BD-POO/src/control/CtrlTableProduto.java
@@ -4,11 +4,6 @@ import java.sql.SQLException;
 import java.util.List;
 import entity.Produto;
 
-/**
- * Interface que possui metodos a serem implementados, de tabela.
- * @author Hury
- *
- */
 
 public interface CtrlTableProduto {
 

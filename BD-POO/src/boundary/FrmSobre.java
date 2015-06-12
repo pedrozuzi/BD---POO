cfc39cfb44c6e3cd711810b93033b5146c15aa1c
pepeu07 <Pedro Afonso@PEDRO>
2015-06-12 11:57:10 -0300

    configuracoes

--- BD-POO/src/boundary/FrmSobre.java
+++ BD-POO/src/boundary/FrmSobre.java
@@ -11,6 +11,7 @@ import javax.swing.JTextPane;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 
+import entity.Usuario;
 import util.ConfiguracoesTela;
 
 import java.awt.Window.Type;
@@ -18,56 +19,58 @@ import java.awt.Window.Type;
 
 public class FrmSobre {
 
-	private JFrame janela = new JFrame("Sobre");
+	private JFrame janela;
 	private ConfiguracoesTela configTela;
+	private JTextPane txtpnInformacoes;
+	private JPanel panPrincipal;
+	private JButton btnOk;
+	private JLabel lblBarraVermelhaEmCima;
+	private JLabel lblBarraVermelhaEmBaixo;
+	private JLabel lblBarraCinzaEmCima;
+	private JLabel lblBarraCinzaEmBaixo;
+	private JScrollPane spInformacoes;
 
-	public FrmSobre(){
+	public FrmSobre(Usuario u){
 		
-		JPanel panPrincipal = new JPanel();
+		janela = new JFrame("Sobre");
+		
+		panPrincipal = new JPanel();
 		panPrincipal.setBackground(Color.WHITE);
-		JPanel panForm = new JPanel();
-		janela.setType(Type.UTILITY);
-		janela.setResizable(false);
-		
 		janela.setContentPane( panPrincipal);
 		panPrincipal.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Ok");
-		btnNewButton.setBounds(145, 264, 89, 23);
-		panPrincipal.add(btnNewButton);
+		btnOk = new JButton("Ok");
+		btnOk.setBounds(145, 264, 89, 23);
+		panPrincipal.add(btnOk);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste.png")));
-		label.setBounds(0, 11, 362, 14);
-		panPrincipal.add(label);
+		lblBarraVermelhaEmCima = new JLabel("");
+		lblBarraVermelhaEmCima.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste.png")));
+		lblBarraVermelhaEmCima.setBounds(0, 11, 362, 14);
+		panPrincipal.add(lblBarraVermelhaEmCima);
 		
-		JLabel label_1 = new JLabel("");
-		label_1.setBounds(286, 217, 46, 14);
-		panPrincipal.add(label_1);
+		lblBarraVermelhaEmBaixo = new JLabel("");
+		lblBarraVermelhaEmBaixo.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste.png")));
+		lblBarraVermelhaEmBaixo.setBounds(22, 307, 362, 14);
+		panPrincipal.add(lblBarraVermelhaEmBaixo);
 		
-		JLabel label_2 = new JLabel("");
-		label_2.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste.png")));
-		label_2.setBounds(22, 307, 362, 14);
-		panPrincipal.add(label_2);
+		lblBarraCinzaEmCima = new JLabel("");
+		lblBarraCinzaEmCima.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste2.png")));
+		lblBarraCinzaEmCima.setBounds(41, 25, 343, 14);
+		panPrincipal.add(lblBarraCinzaEmCima);
 		
-		JLabel label_3 = new JLabel("");
-		label_3.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste2.png")));
-		label_3.setBounds(41, 25, 343, 14);
-		panPrincipal.add(label_3);
+		lblBarraCinzaEmBaixo = new JLabel("");
+		lblBarraCinzaEmBaixo.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste2.png")));
+		lblBarraCinzaEmBaixo.setBounds(0, 293, 343, 14);
+		panPrincipal.add(lblBarraCinzaEmBaixo);
 		
-		JLabel label_4 = new JLabel("");
-		label_4.setIcon(new ImageIcon(FrmSobre.class.getResource("/img/teste2.png")));
-		label_4.setBounds(0, 293, 343, 14);
-		panPrincipal.add(label_4);
+		spInformacoes = new JScrollPane();
+		spInformacoes.setBounds(53, 50, 279, 180);
+		panPrincipal.add(spInformacoes);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(53, 50, 279, 180);
-		panPrincipal.add(scrollPane);
-		
-		JTextPane txtpnHuryGabrielPedro = new JTextPane();
-		scrollPane.setViewportView(txtpnHuryGabrielPedro);
-		txtpnHuryGabrielPedro.setEditable(false);
-		txtpnHuryGabrielPedro.setText("Projeto desenvolvido na institui\u00E7\u00E3o FATEC da zona leste, no 4\u00BA semestre do curso de an\u00E1lise e desenvolvimento de sistemas sob orienta\u00E7\u00E3o dos prof(a) ms. Antonio e Leandro.\r\n\r\n               \t  Alunos:\r\n\tHury Gabriel\r\n\tPedro Afonso\t\r\n\tPedro Zuzi\r\n\r\n ");
+		txtpnInformacoes = new JTextPane();
+		spInformacoes.setViewportView(txtpnInformacoes);
+		txtpnInformacoes.setEditable(false);
+		txtpnInformacoes.setText("Projeto desenvolvido na institui\u00E7\u00E3o FATEC da zona leste, no 4\u00BA semestre do curso de an\u00E1lise e desenvolvimento de sistemas sob orienta\u00E7\u00E3o dos prof(a) ms. Antonio e Leandro.\r\n\r\n               \t  Alunos:\r\n\tHury Gabriel\r\n\tPedro Afonso\t\r\n\tPedro Zuzi\r\n\r\n ");
 		
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -75,11 +78,17 @@ public class FrmSobre {
 		janela.setSize(400,371);
 		configTela.iconeBarra(janela);
 		configTela.tamanho(janela);
+		
+		btnOk.addActionListener( e -> {
+			janela.dispose();
+			janela = null;
+			new FrmPrincipal(u);
+		});
 	}
 	
 	
 	public static void main(String[] args) {
 
-		new FrmSobre();
+		new FrmSobre(new Usuario());
 	}
 }

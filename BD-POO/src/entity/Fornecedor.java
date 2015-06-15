c5402559d1d4fd2b22c0abe04416d88359e0728e
Pedro <Pedro@Pedro-PC>
2015-06-15 10:49:38 -0300

    comentarios

--- BD-POO/src/entity/Fornecedor.java
+++ BD-POO/src/entity/Fornecedor.java
@@ -1,22 +1,28 @@
 package entity;
 
+/**
+ * Classe para os objetos do tipo Fornecedor, onde serão contidos
+ * os valores e métodos para o mesmo
+ * @author Pedro Afonso
+ * @author Pedro Zuzi
+ *
+ */
 public class Fornecedor extends Pessoa{
 	
-	private String empresa;
-	
-	public String getEmpresa() {
-		return empresa;
-	}
-	public void setEmpresa(String empresa) {
-		this.empresa = empresa;
-	}
-	
+	/**
+	 * Método para retorno do nome do fornecedor
+	 * @return Nome do Fornecedor
+	 */
 	@Coluna(nome="Nome", posicao=0)
 	@Override
 	public String getNome() {
 		return super.getNome();
 	}
 	
+	/**
+	 * Método para retorno do telefone do Fornecedor
+	 * @return telefone
+	 */
 	@Coluna(nome="Telefone", posicao=1)
 	@Override
 	public int getTelefone() {
--- BD-POO/src/entity/Pessoa.java
+++ BD-POO/src/entity/Pessoa.java
@@ -1,5 +1,11 @@
 package entity;
 
+/**
+ * Classe para os objetos do tipo Pessoa, onde serão contidos
+ * os valores e métodos para o mesmo
+ * @author Pedro Afonso
+ *
+ */
 public class Pessoa {
 	
 	private int id;
@@ -7,27 +13,66 @@ public class Pessoa {
 	private int telefone;
 	private String nome;
 	
+	/**
+	 * Método para retorno do id
+	 * @return id
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * Método para setar o valor do id
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	/**
+	 * Método para retorno do idTipo da pessoa
+	 * @return tipo do id da pessoa
+	 */
 	public int getIdTipo() {
 		return idTipo;
 	}
+	
+	/**
+	 * Método para setar o valor do tipo de id da pessoa
+	 * @param idTipo
+	 */
 	public void setIdTipo(int idTipo) {
 		this.idTipo = idTipo;
 	}
+	
+	/**
+	 * Método para retorno do nome da pessoa
+	 * @return nome
+	 */
 	public String getNome() {
 		return nome;
 	}
+	
+	/**
+	 * Método para setar o nome da pessoa
+	 * @param nome
+	 */
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	/**
+	 * Método para retorno do telefone da pessoa
+	 * @return telefone
+	 */
 	public int getTelefone() {
 		return telefone;
 	}
+	
+	/**
+	 * Método para setar o telefone da pessoa
+	 * @param telefone
+	 */
 	public void setTelefone(int telefone) {
 		this.telefone = telefone;
 	}

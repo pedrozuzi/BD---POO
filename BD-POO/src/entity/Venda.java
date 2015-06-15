0d725f8901298efcf8e919688b06669c318b4ad4
pepeu07 <Pedro Afonso@PEDRO>
2015-06-15 18:47:42 -0300

    javadoc

--- BD-POO/src/entity/Venda.java
+++ BD-POO/src/entity/Venda.java
@@ -14,34 +14,70 @@ public class Venda {
 	private int idfuncionario;
 	private double total;
 	
+	/**
+	 * Método para retorno do id da venda
+	 * @return valor do id
+	 */
 	@Coluna(nome = "id", posicao = 0)
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * Método para setar o id da venda
+	 * @param id valor do id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Método para retorno do id do cliente
+	 * @return valor do id
+	 */
 	@Coluna(nome = "idcliente", posicao = 1)
 	public int getIdcliente() {
 		return idcliente;
 	}
+	
+	/**
+	 * Método para setar o valor do id do cliente
+	 * @param idcliente valor do id
+	 */
 	public void setIdcliente(int idcliente) {
 		this.idcliente = idcliente;
 	}
 	
+	/**
+	 * Método para retorno do id do funcionario
+	 * @return valor do id
+	 */
 	@Coluna(nome = "idfuncionario", posicao = 2)
 	public int getIdfuncionario() {
 		return idfuncionario;
 	}
+	
+	/**
+	 * Método para setar o 
+	 * @param idfuncionario
+	 */
 	public void setIdfuncionario(int idfuncionario) {
 		this.idfuncionario = idfuncionario;
 	}
 	
+	/**
+	 * Método para retorno do total da venda
+	 * @return valor total 
+	 */
 	@Coluna(nome = "total", posicao = 3)
 	public double getTotal() {
 		return total;
 	}
+	
+	/**
+	 * Método para setar o total da venda
+	 * @param total valor total
+	 */
 	public void setTotal(double total) {
 		this.total = total;
 	}
--- BD-POO/src/entity/VendaProduto.java
+++ BD-POO/src/entity/VendaProduto.java
@@ -2,6 +2,12 @@ package entity;
 
 import java.util.Date;
 
+/**
+ * Classe para objeto do tipo VendaProduto com seus valores e
+ * métodos necessário
+ * @author Hury
+ *
+ */
 public final class VendaProduto {
 
 	private Date data;
@@ -9,38 +15,70 @@ public final class VendaProduto {
 	private int idproduto;
 	private int quantidade;
 
+	/**
+	 * Método para retorno da data da venda
+	 * @return data da venda
+	 */
 	@Coluna(nome = "data", posicao = 0)
 	public Date getData() {
 		return data;
 	}
 
+	/**
+	 * Método para setar a data da venda
+	 * @param data
+	 */
 	public void setData(Date data) {
 		this.data = data;
 	}
 
+	/**
+	 * Método para retorno do id da venda
+	 * @return valor do id da venda
+	 */
 	@Coluna(nome = "idvenda", posicao = 1)
 	public int getIdvenda() {
 		return idvenda;
 	}
 
+	/**
+	 * Método para setar o valor do id da venda
+	 * @param idvenda valor do id
+	 */
 	public void setIdvenda(int idvenda) {
 		this.idvenda = idvenda;
 	}
 
+	/**
+	 * Método para retorno do id do produto
+	 * @return
+	 */
 	@Coluna(nome = "idproduto", posicao = 2)
 	public int getIdproduto() {
 		return idproduto;
 	}
 
+	/**
+	 * Método para setar o valor do id do produto
+	 * @param idproduto
+	 */
 	public void setIdproduto(int idproduto) {
 		this.idproduto = idproduto;
 	}
 
+	/**
+	 * Método para retorno da quantidade da venda
+	 * @return valor da quantidade
+	 */
 	@Coluna(nome = "idquantidade", posicao = 3)
 	public int getQuantidade() {
 		return quantidade;
 	}
 
+	/**
+	 * Método para setar o valor da quantidade da venda
+	 * @param quantidade valor da quantidade
+	 */
 	public void setQuantidade(int quantidade) {
 		this.quantidade = quantidade;
 	}

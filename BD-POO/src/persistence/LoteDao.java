c1959c56109009e1727219649d3b2cd6303be13f
Pedro <Pedro@Pedro-PC>
2015-06-15 20:26:47 -0300

    c

--- BD-POO/src/persistence/LoteDao.java
+++ BD-POO/src/persistence/LoteDao.java
@@ -13,13 +13,45 @@ import entity.Lote;
 
 public interface LoteDao {
 	
+	/**
+	 * Método para inserção de um novo Lote ao
+	 * banco de dados
+	 * @param lot
+	 * @return 
+	 * @throws SQLException exceção do banco de dados
+	 */
 	int insereLote(Lote lot) throws SQLException;
 
+	/**
+	 * Método para atualização de um Lote no banco
+	 * de dados
+	 * @param lot
+	 * @throws SQLException exceção do banco de dados
+	 */
 	void atualizaLote(Lote lot) throws SQLException;
 
+	/**
+	 * Método para exclusão de um Lote no banco
+	 * de dados
+	 * @param lot
+	 * @throws SQLException exceção do banco de dados
+	 */
 	void excluiLote(Lote lot) throws SQLException;
 
+	/**
+	 * Método para consulta de um lote
+	 * @param lot
+	 * @return
+	 * @throws SQLException exceção do banco de dados
+	 */
 	Lote consultaLote(Lote lot) throws SQLException;
 
+	/**
+	 * Método para buscar do banco de dados Objetos
+	 * do tipo Lote
+	 * @param id
+	 * @return List<Lote> contendo as informações
+	 * @throws SQLException exceção do banco de dados
+	 */
 	List<Lote> listaLote(int id) throws SQLException;
 }
--- BD-POO/src/persistence/LoteProdutoDao.java
+++ BD-POO/src/persistence/LoteProdutoDao.java
@@ -5,17 +5,50 @@ import java.util.List;
 
 import entity.LoteProduto;
 
-
-
+/**
+ * Interface com os metodos de Lote de um Produto a serem implementados
+ * @author Hury
+ *
+ */
 public interface LoteProdutoDao {
 	
+	/**
+	 * Método para inserção de um novo Lote e Produto ao
+	 * banco de dados
+	 * @param lotProd
+	 * @throws SQLException
+	 */
 	void insereLoteProduto(LoteProduto lotProd) throws SQLException;
 
+	/**
+	 * Método para atualização de um Lote e Produto no banco
+	 * de dados
+	 * @param lotProd
+	 * @throws SQLException
+	 */
 	void atualizaLoteProduto(LoteProduto lotProd) throws SQLException;
 
+	/**
+	 * Método para exclusão de um Lote e Produto no banco
+	 * de dados
+	 * @param lotProd
+	 * @throws SQLException
+	 */
 	void excluiLoteProduto(LoteProduto lotProd) throws SQLException;
 
+	/**
+	 * Método para consulta de um Lote e um Produto
+	 * @param lotProd
+	 * @return Objeto LoteProduto
+	 * @throws SQLException
+	 */
 	LoteProduto consultaLoteProduto(LoteProduto lotProd) throws SQLException;
 
+	/**
+	 * Método para buscar do banco de dados Objetos
+	 * do tipo Lote e Produto
+	 * @return List<LoteProduto> contendo as informações
+	 * @throws SQLException
+	 */
 	List<LoteProduto> listaLoteProduto() throws SQLException;
 }

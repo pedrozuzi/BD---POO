682e30d063b0ee34052a24987d820a2390994113
pepeu07 <Pedro Afonso@PEDRO>
2015-06-15 21:39:55 -0300

    a

--- BD-POO/src/boundary/FrmFuncionario.java
+++ BD-POO/src/boundary/FrmFuncionario.java
@@ -15,7 +15,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JMenu;
